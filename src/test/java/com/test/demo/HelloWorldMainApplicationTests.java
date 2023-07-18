758bc52f91a2135389958908f5dd40df22f473d2
程陈瑞 <1804075821@qq.com>
2023-07-18 09:30:24 +0800

    save

--- src/test/java/com/test/demo/HelloWorldMainApplicationTests.java
+++ src/test/java/com/test/demo/HelloWorldMainApplicationTests.java
@@ -4,10 +4,13 @@ package com.test.demo;
 import com.test.demo.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 /**
  * Spring Boot 单元测试
@@ -23,6 +26,9 @@ public class HelloWorldMainApplicationTests {
     @Autowired
     private ApplicationContext ioc;
 
+    // 记录器
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void contextLoads() {
         System.out.println("person:" + person);
@@ -31,6 +37,21 @@ public class HelloWorldMainApplicationTests {
     @Test
     public void testHelloService() {
         System.out.println(ioc.containsBean("helloService"));
+    }
+
+    @Test
+    public void testLogging() {
+        /*
+         * 日志的级别，由低到高
+         * 可以调整输出日志的级别，日志就只会在这个级别以后的高级别生效
+         * SpringBoot默认给我们使用的是info级别的日志
+         * 可以通过配置文件调整level等级
+         */
+        logger.trace("这是trace日志...");
+        logger.debug("这是debug日志...");
+        logger.info("这是info日志...");
+        logger.warn("这是warn日志...");
+        logger.error("这是error日志...");
     }
 
 
